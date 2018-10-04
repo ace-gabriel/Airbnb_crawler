3cb809c35fb4e125d91f14d10a6cf81bce90d7e1
ace-gabriel <tcyin@ucdavis.edu>
2018-10-04 11:57:58 -0400

    Airbnb Webcrawler

--- Airbnb_crawler/spiders/Airbnb.py
+++ Airbnb_crawler/spiders/Airbnb.py
@@ -1,4 +1,5 @@
 import scrapy
+import numpy as np
 
 description = "__tutorial__"
 author = "__Gabriel__"
@@ -10,12 +11,8 @@ class Crawler(scrapy.Spider):
 
     def start_requests(self):
         # define urls to be crawled
-        urls = [
-        'http://quotes.toscrape.com/',
-        ]
-
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        URL = "https://www.airbnb.com/s/New-York--NY--United-States/all?refinement_paths%5B%5D=%2Ffor_you&query=New%20York%2C%20NY%2C%20United%20States&place_id=ChIJOwg_06VPwokRYv534QaPC8g"
+        yield scrapy.Request(url=URL , callback=self.parse)
 
     def parse_info(self, response):
         # store two files, quotes-1.html quotes-2.html
@@ -49,3 +46,34 @@ class Crawler(scrapy.Spider):
 
             print(next_page + 'absolute')
             yield scrapy.Request(next_page, callback=self.parse)
+
+
+class Airbnb(scrapy.Spider):
+    # airbnb crawler
+    name = "airbnb_crawler"
+
+    def start_requests(self):
+        URL = "https://www.airbnb.com/rooms/20532901?location=New%20York%2C%20NY%2C%20United%20States&adults=1&children=0&infants=0&guests=1&check_in=2018-10-04&check_out=2018-10-16&s=90kv0Krj"
+        yield scrapy.Request(url=URL , callback=self.parse)
+
+    def parse(self, response):
+        # type of the room
+        type = response.css('span[class="_1hh2h7tb"] span::text').extract_first()
+        # title of the room
+        title = response.css('h1[class="_1xu9tpch"]::text').extract_first()
+        # '1 bedroom' '2 private baths' '3 beds' '5 guests'
+        infos = np.array(response.xpath("//span[@class = '_fgdupie']/text()").extract()[:4])
+        # print(len(infos), infos)
+        guests = infos[0].split(' ')[0]
+        bedroom = infos[1].split(' ')[0]
+        beds = infos[2].split(' ')[0]
+        baths = infos[3].split(' ')[0]
+        # yield json result
+        yield {
+              'type' : type,
+              'title' : title,
+              'guests' : guests,
+              'bedroom' : bedroom,
+              'beds' : beds,
+              'baths' : baths
+        }
